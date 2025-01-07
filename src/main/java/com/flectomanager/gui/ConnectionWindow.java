78058d8ddfef084777ce067cd03b27bedfd7d2ea
MusiJVR <mousejava102@gmail.com>
2025-01-07 11:31:50 +0400

    Added the font to the project resources

--- src/main/java/com/flectomanager/gui/ConnectionWindow.java
+++ src/main/java/com/flectomanager/gui/ConnectionWindow.java
@@ -5,7 +5,6 @@ import com.flectomanager.util.LocalizationManager;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
